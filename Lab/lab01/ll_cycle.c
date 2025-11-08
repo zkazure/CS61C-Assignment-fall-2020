54a1ca63a7f9837806291b4c5f6544a231cf2ae5
Kazure Zheng <kazure@debian>
2025-11-08 21:03:23 +0800

    finish lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) {
+        return 0;        
+    }
+
+    
+    node *tortoise = head ;
+    node *hare = head;
+
+    while (hare!=NULL && hare->next!=NULL) {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
+    
     return 0;
 }
