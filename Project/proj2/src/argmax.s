cbaa1daacf151e7d79400475765c6f115b48b33a
Kazure Zheng <kazure@debian>
2025-12-11 18:38:46 +0800

    finish task2

--- Project/proj2/src/argmax.s
+++ Project/proj2/src/argmax.s
@@ -1,34 +1,57 @@
-.globl argmax
+    .globl argmax
 
-.text
-# =================================================================
-# FUNCTION: Given a int vector, return the index of the largest
-#	element. If there are multiple, return the one
-#	with the smallest index.
-# Arguments:
-# 	a0 (int*) is the pointer to the start of the vector
-#	a1 (int)  is the # of elements in the vector
-# Returns:
-#	a0 (int)  is the first index of the largest element
-# Exceptions:
-# - If the length of the vector is less than 1,
-#   this function terminates the program with error code 77.
-# =================================================================
+    .text
+    # =================================================================
+    # FUNCTION: Given a int vector, return the index of the largest
+    #	element. If there are multiple, return the one
+    #	with the smallest index.
+    # Arguments:
+    # 	a0 (int*) is the pointer to the start of the vector
+    #	a1 (int)  is the # of elements in the vector
+    # Returns:
+    #	a0 (int)  is the first index of the largest element
+    # Exceptions:
+    # - If the length of the vector is less than 1,
+    #   this function terminates the program with error code 77.
+    # =================================================================
+
 argmax:
-
+    addi sp, sp, -8
+    sw s0, 0(sp)
+    sw s1, 4(sp)
     # Prologue
 
-
+    li t0, 1
+    blt a1, t0, invalid_length
+    
+    mv s0, a0
+    mv s1, a1
+    add t0, zero, s0
+    add t1, zero, zero
+    lw t2, 0(sp)
+    add t3, zero, zero
 loop_start:
-
+    beq t1, s1, loop_end
+    lw t4, 0(t0)
+    bge t4, t2, loop_continue
+    add t2, zero, t4
+    add t3, zero, t1
 
 loop_continue:
-
-
-loop_end:
+    addi t1, t1, 1
+    addi t0, t0, 4
+    j loop_start
     
+loop_end:
+    mv a0, t3
 
     # Epilogue
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    addi, sp, sp, 8
 
     ret
+
+invalid_length:
+    li a1, 77
+    jal exit2
--- Project/proj2/unittests/unittests.py
+++ Project/proj2/unittests/unittests.py
@@ -52,15 +52,20 @@ class TestArgmax(TestCase):
     def test_simple(self):
         t = AssemblyTest(self, "argmax.s")
         # create an array in the data section
-        raise NotImplementedError("TODO")
+        # raise NotImplementedError("TODO")
         # TODO
         # load address of the array into register a0
+        array0 = t.array([1, -2, 3, -4, 5, -6, 7, -8, 9])
+        t.input_array("a0", array0)
         # TODO
         # set a1 to the length of the array
+        t.input_scalar("a1", len(array0))
         # TODO
         # call the `argmax` function
+        t.call("argmax")
         # TODO
         # check that the register a0 contains the correct output
+        t.check_scalar("a0", 7)
         # TODO
         # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
         t.execute()
