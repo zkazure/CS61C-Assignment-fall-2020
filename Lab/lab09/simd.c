82e769901e4302f5f737e0a0571f065d39460f50
Kazure Zheng <kazure@debian>
2026-02-01 19:35:36 +0800

    finish exercise 2

--- Lab/lab09/simd.c
+++ Lab/lab09/simd.c
@@ -57,10 +57,24 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
     long long int result = 0; // This is where you should put your final result!
     /* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
 
+    int outer_sum[4] = {0, 0, 0, 0};
     for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
-
+        __m128i inner_sum = _mm_setzero_si128();
+        unsigned int i = 0;
+        for (; i + 3 < NUM_ELEMS; i += 4) {
+            __m128i vec = _mm_loadu_si128((__m128i *)vals + i);
+            __m128i mask = _mm_cmpgt_epi32(vec, _127);
+            __m128i vec_masked = _mm_and_si128(vec, mask);
+            inner_sum = _mm_add_epi32(inner_sum, vec_masked);
+        }
+        _mm_storeu_si128((__m128i *)outer_sum, inner_sum);
+        result += outer_sum[0] + outer_sum[1] + outer_sum[2] + outer_sum[3];
         /* You'll need a tail case. */
+        for (; i < NUM_ELEMS; ++i) {
+            if (vals[i] > 127)
+                result += vals[i];
+        }
     }
     clock_t end = clock();
     printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
