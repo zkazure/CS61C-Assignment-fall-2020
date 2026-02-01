4e6fdd82b33032a4cf4610b1fbb878783a09c1f3
Kazure Zheng <kazure@debian>
2026-02-01 20:50:34 +0800

    pass exercise 3

--- Lab/lab09/simd.c
+++ Lab/lab09/simd.c
@@ -57,16 +57,16 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
     long long int result = 0; // This is where you should put your final result!
     /* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
 
-    int outer_sum[4] = {0, 0, 0, 0};
+    int outer_sum[4];
     for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
         __m128i inner_sum = _mm_setzero_si128();
         unsigned int i = 0;
         for (; i + 3 < NUM_ELEMS; i += 4) {
-            __m128i vec = _mm_loadu_si128((__m128i *)vals + i);
+            __m128i vec = _mm_loadu_si128((__m128i *)(vals + i));
             __m128i mask = _mm_cmpgt_epi32(vec, _127);
-            __m128i vec_masked = _mm_and_si128(vec, mask);
-            inner_sum = _mm_add_epi32(inner_sum, vec_masked);
+            vec = _mm_and_si128(vec, mask);
+            inner_sum = _mm_add_epi32(inner_sum, vec);
         }
         _mm_storeu_si128((__m128i *)outer_sum, inner_sum);
         result += outer_sum[0] + outer_sum[1] + outer_sum[2] + outer_sum[3];
@@ -85,11 +85,51 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     clock_t start = clock();
     __m128i _127 = _mm_set1_epi32(127);
     long long int result = 0;
+
+    int outer_sum[4];
     for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* COPY AND PASTE YOUR sum_simd() HERE */
         /* MODIFY IT BY UNROLLING IT */
+        __m128i inner_sum0 = _mm_setzero_si128();
+        __m128i inner_sum1 = _mm_setzero_si128();
+        __m128i inner_sum2 = _mm_setzero_si128();
+        __m128i inner_sum3 = _mm_setzero_si128();
+        unsigned int i = 0;
+        for (; i + 15 < NUM_ELEMS; i += 16) {
+            __m128i vec, mask;
+
+            vec = _mm_loadu_si128((__m128i *)(vals + i));
+            mask = _mm_cmpgt_epi32(vec, _127);
+            vec = _mm_and_si128(vec, mask);
+            inner_sum0 = _mm_add_epi32(inner_sum0, vec);
+
+            vec = _mm_loadu_si128((__m128i *)(vals + i + 4));
+            mask = _mm_cmpgt_epi32(vec, _127);
+            vec = _mm_and_si128(vec, mask);
+            inner_sum1 = _mm_add_epi32(inner_sum1, vec);
+
+            vec = _mm_loadu_si128((__m128i *)(vals + i + 8));
+            mask = _mm_cmpgt_epi32(vec, _127);
+            vec = _mm_and_si128(vec, mask);
+            inner_sum2 = _mm_add_epi32(inner_sum2, vec);
+
+            vec = _mm_loadu_si128((__m128i *)(vals + i + 12));
+            mask = _mm_cmpgt_epi32(vec, _127);
+            vec = _mm_and_si128(vec, mask);
+            inner_sum3 = _mm_add_epi32(inner_sum3, vec);
+        }
+        __m128i inner_sum =
+            _mm_add_epi32(_mm_add_epi32(inner_sum0, inner_sum1),
+                          _mm_add_epi32(inner_sum2, inner_sum3));
+        _mm_storeu_si128((__m128i *)outer_sum, inner_sum);
+        result += outer_sum[0] + outer_sum[1] + outer_sum[2] + outer_sum[3];
 
         /* You'll need 1 or maybe 2 tail cases here. */
+        for (; i < NUM_ELEMS; ++i) {
+            if (vals[i] > 127) {
+                result += vals[i];
+            }
+        }
     }
     clock_t end = clock();
     printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
