ae8e9056bb779ef0f0914ecc1455e6a740b95333
Kazure Zheng <kazure@debian>
2026-02-03 12:17:58 +0800

    some modify

--- Lab/lab10/hello.c
+++ Lab/lab10/hello.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <omp.h>
+#include <stdio.h>
 
 int main() {
-	#pragma omp parallel
-	{
-		int thread_ID = omp_get_thread_num();
-		printf(" hello world %d\n", thread_ID);
-	}
+#pragma omp parallel
+    {
+        int thread_ID = omp_get_thread_num();
+
+        printf(" hello world %d\n", thread_ID);
+    }
 }
